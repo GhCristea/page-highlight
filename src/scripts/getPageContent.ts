49ed758d18b1989237c1e0abb86a4d60dca36d44
Gheorghita Cristea <gcristea73@gmail.com>
2026-01-17 22:20:03 +0000

    feat(getPageContent): enhance content filtering by removing additional elements and patterns

--- src/scripts/getPageContent.ts
+++ src/scripts/getPageContent.ts
@@ -2,9 +2,22 @@ const getPageContent = async () => {
   const clone = document.body.cloneNode(true) as HTMLElement;
 
   const elementsToRemove = clone.querySelectorAll(
-    "script, style, noscript, svg, img, video, audio, iframe, canvas"
+    "script, style, noscript, svg, img, video, audio, iframe, canvas, " +
+    "nav, header, footer, aside, form, button, input, select, textarea, " +
+    "embed, object, applet, area, map, track, source"
   );
+
   elementsToRemove.forEach((el) => el.remove());
+
+  const negativePatterns = /(comment|meta|footer|footnote|ad|advertisement|sidebar|menu|navigation|nav|social|share|widget)/i;
+  const allElements = clone.querySelectorAll("*");
+  allElements.forEach((el) => {
+    const className = el.className?.toString() || "";
+    const id = el.id || "";
+    if (negativePatterns.test(className) || negativePatterns.test(id)) {
+      el.remove();
+    }
+  });
 
   return clone.innerHTML;
 };
