0ecac94321fb8a611045b1132c08c573f7f76e6e
Gheorghita Cristea <gcristea73@gmail.com>
2026-01-21 23:01:23 +0000

    fix(offscreen): sentences count less than 3

--- src/offscreen/getRelevantText.ts
+++ src/offscreen/getRelevantText.ts
@@ -5,30 +5,38 @@ import { SENTENCE_IMPORTANCE } from "../lib/constants";
 const nlp = winkNLP(model, ["sbd", "pos"]);
 
 const getRelevantText = (textContent: string): Sentence[] => {
-  const top = 0.2;
+  const top = 0.2; // 20%
+  const minImportance = 0.01; // 1%
+
+  const minCount = SENTENCE_IMPORTANCE.length; // 3
+  const [High, Med, Low] = SENTENCE_IMPORTANCE;
+
   const doc = nlp.readDoc(textContent);
 
-  const descOrderImportance = (
-    doc.out(nlp.its.sentenceWiseImportance) as SentenceImportance[]
-  ).sort((a, b) => b.importance - a.importance);
+  const descOrderImportance = (doc.out(nlp.its.sentenceWiseImportance) as SentenceImportance[])
+    .filter((s) => s.importance >= minImportance)
+    .sort((a, b) => b.importance - a.importance);
 
-  const topSentences = descOrderImportance.slice(0, descOrderImportance.length * top);
+  const count = descOrderImportance.length;
 
+  if (count <= minCount) {
+    return descOrderImportance.map((si, index) => ({
+      txt: doc.sentences().itemAt(si.index).out(),
+      level: SENTENCE_IMPORTANCE[index],
+    }));
+  }
+
+  const sliceAt = Math.ceil(Math.max(count * top, minCount));
+  const topSentences = descOrderImportance.slice(0, sliceAt);
   const splitIndex = Math.floor(topSentences.length / 3);
+
   const high = topSentences[splitIndex].importance;
   const medium = topSentences[splitIndex * 2].importance;
 
   const sentences = topSentences.map((si) => ({
     txt: doc.sentences().itemAt(si.index).out(),
-    level:
-      si.importance > high
-        ? SENTENCE_IMPORTANCE[0]
-        : si.importance > medium
-        ? SENTENCE_IMPORTANCE[1]
-        : SENTENCE_IMPORTANCE[2],
+    level: si.importance >= high ? High : si.importance >= medium ? Med : Low,
   }));
-
-  doc.sentences().itemAt;
 
   return sentences;
 };
